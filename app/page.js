b93bd52130d766f1e03c911099eacd9929e73641
Jordan Cortes <hello@jordancortes.com>
2023-09-13 09:59:25 -0600

    change background image

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Image src="/images/background.jpg" alt="Background" className="grayscale" fill={true} priority />
-      <div className="absolute w-full h-full bg-neutral-900 opacity-60">hello</div>
+      <Image src="/images/background.jpg" alt="Background" fill={true} priority />
     </main>
   );
 }
